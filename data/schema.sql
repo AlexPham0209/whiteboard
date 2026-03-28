1f604fdc7d9af90f9eca6e6a024f996465d65228
AlexPham0209 <AlexLPham0209@gmail.com>
2026-03-28 11:20:20 -0400

    Finished schema update

--- data/schema.sql
+++ data/schema.sql
@@ -2,12 +2,23 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TYPE draw_mode AS ENUM ('draw', 'erase');
 CREATE TYPE pen_color AS ENUM ('black', 'red', 'blue');
 
+-- Rooms
 CREATE TABLE rooms (
     id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
     room_code VARCHAR(5) UNIQUE NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
+CREATE INDEX idx_room_code ON rooms (room_code);
+
+-- Users
+CREATE TABLE users (
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(), 
+    username UNIQUE VARCHAR(16),
+    password VARCHAR(16)
+);
+
+-- Members
 CREATE TABLE members (
     id uuid PRIMARY KEY DEFAULT uuid_generate_v4(), 
     user_id uuid NOT NULL,
@@ -15,7 +26,7 @@ CREATE TABLE members (
     joined_at TIMESTAMP NOT NULL DEFAULT NOW(),
 
     CONSTRAINT fk_user 
-        FOREIGN KEY (user_id) REFERENCES users(id)
+        FOREIGN KEY (user_id) REFERENCES users(id),
     
     CONSTRAINT fk_room 
         FOREIGN KEY (room_id) REFERENCES rooms(id)
@@ -24,28 +35,31 @@ CREATE TABLE members (
     UNIQUE (user_id, room_id)
 );
 
-CREATE TABLE users (
-    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(), 
-    username UNIQUE VARCHAR(16),
-    password VARCHAR(16)
-);
+CREATE INDEX idx_user_id ON members (user_id);
+CREATE INDEX idx_room_id ON members (room_id);
+CREATE INDEX idx_joined_at ON members (joined_at);
 
+-- Lines
 CREATE TABLE lines (
-    -- id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     id uuid PRIMARY KEY DEFAULT uuid_generate_v4(), 
     room_id uuid NOT NULL,
-    -- user_id bigint NOT NULL,
-    username VARCHAR(255) NOT NULL,
+    user_id uuid NOT NULL,
     draw_mode draw_mode,
     color pen_color,
     brush_size int DEFAULT 5,
     points float[],
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
 
+    CONSTRAINT fk_user 
+        FOREIGN KEY (user_id) REFERENCES users(id),
+
     CONSTRAINT fk_room 
         FOREIGN KEY (room_id) REFERENCES rooms(id)
-        ON DELETE CASCADE
+        ON DELETE CASCADE,
 
-    -- CONSTRAINT fk_user 
-    --     FOREIGN KEY (user_id) REFERENCES users(id)
+    UNIQUE (room_id, user_id)
 );
+
+CREATE INDEX idx_user_id ON lines (user_id);
+CREATE INDEX idx_room_id ON lines (room_id);
+CREATE INDEX idx_created_at ON lines (created_at);
