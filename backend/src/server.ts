3640781def66aa0ed19f9588773ee860ec0f88d0
AlexPham0209 <AlexLPham0209@gmail.com>
2026-04-26 11:20:05 -0400

    Added backend load balancing

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,12 +6,17 @@ import registerCanvasHandlers from "./sockets/canvasHandler.js";
 import registerMessageHandlers from "./sockets/messageHandler.js";
 import registerMemberHandlers from "./sockets/memberHandler.js";
 import { app, CORS_CONFIG } from "./app.js";
+import { createAdapter } from "@socket.io/postgres-adapter";
+import pool from "./db/db.js";
 
 export const PORT = process.env.BACKEND_PORT;
 export const server = createServer(app);
 export const io = new Server(server, {
   cors: CORS_CONFIG,
 });
+
+// Helps broadcast events to all servers
+io.adapter(createAdapter(pool));
 
 // Socket middleware
 io.use(authenticateSocket);
